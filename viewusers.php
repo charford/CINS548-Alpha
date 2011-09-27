a4b71e04aba1191a672ed78968caf3d573781228
Casey Harford <casey@caseyharford.com>
2011-09-27 10:05:55 -0700

    made everyone an admin, also updated view users to include user type

--- viewusers.php
+++ viewusers.php
@@ -18,6 +18,7 @@ include 'mysql_settings.php';
 	else $user_type="0";	//set user to lowest privilege(view only)
   
   if($user_type=='2') {
+    $usertypes=array("Regular","Moderator","Admin");
     //output table header
     echo "<table class='results_table'>
             <tr class='results_firstrow'><th>Username</th>
@@ -26,10 +27,11 @@ include 'mysql_settings.php';
                 <th>Phone</th>
                 <th>Address</th>
                 <th>Zip Code</th>
+                <th>User type</th>
             </tr>";
     
     //get user info from database
-    $sql = "SELECT * FROM users ORDER BY username";
+    $sql = "SELECT username,email,f_name,l_name,bday,street_address,zipcode,phone_num,user_type FROM users ORDER BY username";
     $result = mysql_query($sql);
     
     if($result) {
@@ -51,6 +53,7 @@ include 'mysql_settings.php';
                 <td>$phone</td>
                 <td>$street</td>
                 <td>$zipcode</td>
+                <td>".$usertypes[$ut]."</td>
               </tr>";
               
       }

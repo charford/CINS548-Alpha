fc4353b6a0264e6cb78ee1b340a7453bb9dbbc1d
serenabhayani <serena.bhayani@gmail.com>
2011-09-28 22:58:05 -0700

    Edited resetpw.php via GitHub

--- resetpw.php
+++ resetpw.php
@@ -1,1 +1,49 @@
-Reset pw code will go in this file
+<?php
+
+        session_start();
+  include 'mysql_settings.php';
+//        $index_file="index.php";
+               
+$go = $_POST['go'];
+if($go!=1) $form_method="resetpw.php";
+else $form_method="newpswd.php";
+echo "<form action='$form_method' method='post'>";
+
+
+if($go == "1")
+{
+$_SESSION['myusername'] = $_POST['username'];
+$sql = "SELECT * FROM users WHERE (users.username='$_SESSION[myusername]')";
+	$result = mysql_query($sql);
+        if($result) {
+			while($row = mysql_fetch_array($result)) {
+			$_SESSION['security_question']=$row['security_qts'];
+                        $_SESSION['security_answer']=$row['security_ans'];
+                        
+		}
+	} ?>
+        <p>user:<br /><input type="text" name = "myusername " value = <?php echo $_SESSION['myusername'];?> />
+        <p>Answer the security question below:<br />
+            
+<?php echo $_SESSION['security_question']; ?>
+<input type="text" name="security_answer" value="" />
+
+        <input type="submit" name="submitButtonName" border="0"></p>
+<?php }
+else
+{
+    ?>
+    <p>Login below</p>
+<p>Username:<br />
+<input type="text" name="username" value="" /></p>
+<input type="hidden" name="go" value="1" border="0"><input type="submit" name="submitButtonName" border="0">
+
+    <?php
+}
+?>
+
+<?php
+
+//echo $test;
+//echo $row
+?>

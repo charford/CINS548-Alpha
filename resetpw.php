6f739aa3173b3d46f639c7f13b756d68412047b8
serenabhayani <serena.bhayani@gmail.com>
2011-10-02 13:27:32 -0700

    Edited resetpw.php via GitHub

--- resetpw.php
+++ resetpw.php
@@ -1,8 +1,8 @@
 <?php
 
         session_start();
-  include 'mysql_settings.php';
-//        $index_file="index.php";
+	include 'mysql_settings.php';
+        $index_file="index.php";
                
 $go = $_POST['go'];
 if($go!=1) $form_method="resetpw.php";
@@ -12,23 +12,27 @@ echo "<form action='$form_method' method='post'>";
 
 if($go == "1")
 {
-$_SESSION['myusername'] = $_POST['username'];
-$sql = "SELECT * FROM users WHERE (users.username='$_SESSION[myusername]')";
+$user=$_POST['username'];
+$sql = "SELECT * FROM users WHERE username='$user'";
 	$result = mysql_query($sql);
-        if($result) {
-			while($row = mysql_fetch_array($result)) {
-			$_SESSION['security_question']=$row['security_qts'];
-                        $_SESSION['security_answer']=$row['security_ans'];
+        if(!$result) echo "<p><a href=resetpw.php> username not in database, start again</a></p>";
+        
+       else if($result) {
+			while($row = mysql_fetch_array($result))
+                            {
+			    $security_quest=$row['security_qts'];
+                            $sec_ans=$row['security_ans'];
                         
 		}
 	} ?>
-        <p>user:<br /><input type="text" name = "myusername " value = <?php echo $_SESSION['myusername'];?> />
+        <p>user:<br /><input type="text" name = "myusername " value = "<?php echo $user;?>" />
         <p>Answer the security question below:<br />
             
-<?php echo $_SESSION['security_question']; ?>
+<?php echo $security_quest; ?>
 <input type="text" name="security_answer" value="" />
-
-        <input type="submit" name="submitButtonName" border="0"></p>
+<input type="hidden" name="user" value="<?php echo $user;?>">
+<input type="hidden" name="sec_ans" value="<?php echo $sec_ans;?>">
+<input type="submit" name="submitButtonName" border="0"></p>
 <?php }
 else
 {
@@ -44,6 +48,5 @@ else
 
 <?php
 
-//echo $test;
-//echo $row
+
 ?>

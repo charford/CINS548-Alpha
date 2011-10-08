c580b9ba0d69e2b7b384e1cfd8e92cab344a970a
Casey Harford <casey@caseyharford.com>
2011-10-07 23:17:15 -0700

    added validations that Heinz made to validate.php, for email and password

--- validate.php
+++ validate.php
@@ -21,6 +21,10 @@
 			echo "<div id='alert'>password can't be blank</div>";
 			$errors=1;		
 		}
+    elseif (!preg_match("/^(?=^.{7,}$)((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.*$/", $password)) {
+      echo "<div id='alert'>password must be at least 7 chars long with one lower-case, one upper-case char, and one digit</div>";
+      $errors=1;
+    }
 		if($action=='register' && $confirm_password=='') {
 			echo "<div id='alert'>confirm password can't be blank</div>";
 			$errors=1;		
@@ -45,6 +49,10 @@
 			echo "<div id='alert'>email can't be blank</div>";
 			$errors=1;		
 		}
+    elseif (!preg_match("/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i", $email)) {
+      echo "<div id='alert'>email address format is invalid: x@y.z</div>";
+      $errors=1;
+    }
 		if($action=='register' && $security_question=='') {
 			echo "<div id='alert'>security question can't be blank</div>";
 			$errors=1;		

f19326e74fb39dc7f8a81a3a921c3fdeb2a917d7
Casey Harford <casey@caseyharford.com>
2011-10-07 22:42:02 -0700

    removed extra code for validating form into a seperate file for register.php, and edituser.php

--- register.php
+++ register.php
@@ -66,56 +66,7 @@ error_reporting(0);
 		$bday=$birth_year."-".$birth_month."-".$birth_day;
 
 	//validate form data//
-		$errors=0;
-		if($first_name=='') {
-			echo "<div id='alert'>first_name can't be blank</div>";
-			$errors=1;		
-		}
-		if($last_name=='') {
-			echo "<div id='alert'>last name can't be blank</div>";
-			$errors=1;		
-		}
-		if($username=='') {
-			echo "<div id='alert'>username can't be blank</div>";
-			$errors=1;		
-		}
-		if($password=='') {
-			echo "<div id='alert'>password can't be blank</div>";
-			$errors=1;		
-		}
-		if($confirm_password=='') {
-			echo "<div id='alert'>confirm password can't be blank</div>";
-			$errors=1;		
-		}
-		if($password!=$confirm_password) {
-			echo "<div id='alert'>passwords don't match</div>";
-			$errors=1;
-		}
-		if($birth_month=='') {
-			echo "<div id='alert'>birth month can't be blank</div>";
-			$errors=1;		
-		}
-		if($birth_day=='') {
-			echo "<div id='alert'>birth day can't be blank</div>";
-			$errors=1;		
-		}
-		if($birth_year=='') {
-			echo "<div id='alert'>birth year can't be blank</div>";
-			$errors=1;		
-		}
-		if($email=='') {
-			echo "<div id='alert'>email can't be blank</div>";
-			$errors=1;		
-		}
-		if($security_question=='') {
-			echo "<div id='alert'>security question can't be blank</div>";
-			$errors=1;		
-		}
-		if($security_answer=='') {
-			echo "<div id='alert'>security answer can't be blank</div>";
-			$errors=1;		
-		}
-		
+    include 'validate.php';
 	
 		//generate salt, and encrypt it
 		$salt = hash('sha256',date('U'));	//date and time ISO format

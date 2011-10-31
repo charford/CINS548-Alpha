05819da3b4c3b56121d52beaf1cdffb2ac15ee4a
Casey Harford <casey@caseyharford.com>
2011-10-30 21:24:35 -0700

    still working on these stupid prepared statements

--- viewposts.php
+++ viewposts.php
@@ -65,7 +65,7 @@
     //display public only posts
     else $sql = "SELECT title,post_id,date_posted,user_id FROM posts WHERE discussion_id=? AND reply_id = '0' AND privacy=0 ORDER BY date_posted desc";
 
-    $mysqli = new mysqli('132.241.49.7','dbadmin','QjpXfePqGNDfvHB79zYwand5','cins548');
+    $mysqli = new mysqli('132.241.49.7',$read_username,$read_password,'cins548');
     if($stmt = $mysqli->prepare($sql)) {
     $stmt->bind_param("i",$discussion_id);
     $stmt->execute();
@@ -112,17 +112,22 @@
 		$post_id = mysql_real_escape_string($post_id);
 
 		//retrieve post content
-    $mysqli = new mysqli('132.241.49.7','dbadmin','QjpXfePqGNDfvHB79zYwand5','cins548');
-    $sql = "SELECT title,content,user_id,date_posted,discussion_id,post_id,privacy, COUNT(*) as valid FROM posts WHERE post_id=?";
-	  if($stmt = $mysqli->prepare($sql)) {
-    $stmt->bind_param("i",$post_id);
-    $stmt->execute();
-    $stmt->bind_result($post_title,$post_content,$author,$date_posted,$discussion_id,$post_id,$privacy,$valid);
-		while($stmt->fetch()) {
+		$sql = "SELECT *, COUNT(*) as valid FROM posts WHERE post_id='$post_id'";
+		$result = mysql_query($sql);
+		if($result) {
+			$row = mysql_fetch_assoc($result);
+      $valid = $row['valid'];
       if($valid==0) {
         echo "<p>Invalid post requested</p>";
         exit;
       }
+			$post_title = $row['title'];
+			$post_content = $row['content'];
+			$author = $row['user_id'];
+			$date_posted = $row['date_posted'];
+			$discussion_id = $row['discussion_id'];
+			$post_id = $row['post_id'];
+      $privacy = $row['privacy'];
 
       if($logged_in==0 && $privacy!=0) {
         echo "<script>alert('You must be logged in to view this post.');</script>";
@@ -130,89 +135,85 @@
       }
 
 			//retrieve discussion_title
-			$sql = "SELECT title FROM discussions WHERE id=?";
-      if($stmt->prepare($sql)) {
-        $stmt->bind_param("i",$discussion_id);
-        $stmt->execute();
-        $stmt->bind_result($discussion_title);
-			  while($stmt->fetch()) {
-          //don't think i need anything here
-			  }
-      }
+			$sql = "SELECT title FROM discussions WHERE id='$discussion_id'";
+			$result = mysql_query($sql);
+			if($result) {
+				$row = mysql_fetch_assoc($result);
+				$discussion_title=$row['title'];
+			}
 			else $discussion_title="UNDEFINED";		//should never be the case
-	    
-			  //submit response if reply button pressed
-			  if($logged_in==1) {	//must be logged in to post reply
-				  if(isset($_POST['reply'])) {
-					  $author = $_SESSION['myusername'];
-					  $reply_title = $_POST['reply_title'];
-					  $reply_message = $_POST['reply_message'];
+	
+			//submit response if reply button pressed
+			if($logged_in==1) {	//must be logged in to post reply
+				if(isset($_POST['reply'])) {
+					$author = $_SESSION['myusername'];
+					$reply_title = $_POST['reply_title'];
+					$reply_message = $_POST['reply_message'];
 					
-					  //sanitize input
-					  $reply_title = stripslashes($reply_title);
-					  $reply_title = mysql_real_escape_string($reply_title);
-					  $reply_message = stripslashes($reply_message);
-					  $reply_message = mysql_real_escape_string($reply_message);
-            $reply_title = strip_tags($reply_title);
-            $reply_message = strip_tags($reply_message);
+					//sanitize input
+					$reply_title = stripslashes($reply_title);
+					$reply_title = mysql_real_escape_string($reply_title);
+					$reply_message = stripslashes($reply_message);
+					$reply_message = mysql_real_escape_string($reply_message);
+          $reply_title = strip_tags($reply_title);
+          $reply_message = strip_tags($reply_message);
           
         
-					  $sql = "INSERT INTO posts (title,content,user_id,discussion_id,reply_id,date_posted)
-						  VALUES ('$reply_title','$reply_message','$author','$discussion_id','$post_id',NOW())";
-					  $result = mysql_query($sql);
-					  if($result) {
-						  //echo "<p>Reply successful</p>";
-					  }
-					  else echo "<p>An error occurred</p>";
+					$sql = "INSERT INTO posts (title,content,user_id,discussion_id,reply_id,date_posted)
+						VALUES ('$reply_title','$reply_message','$author','$discussion_id','$post_id',NOW())";
+					$result = mysql_query($sql);
+					if($result) {
+						//echo "<p>Reply successful</p>";
+					}
+					else echo "<p>An error occurred</p>";
 					
-				  }
-			  }
-        else echo "<script>alert('You must be logged in to post reply!')</script>";
+				}
+			}
+      else echo "<script>alert('You must be logged in to post reply!')</script>";
 
-			  echo "<a href='?action=viewposts'>View Posts</a> > 
-				  <a href='?action=viewposts&discussion_id=$discussion_id'>$discussion_title</a> > $post_title</p>
+			echo "<a href='?action=viewposts'>View Posts</a> > 
+				<a href='?action=viewposts&discussion_id=$discussion_id'>$discussion_title</a> > $post_title</p>
 
-				  <h2>$post_title</h2>
-				  <p>Author: $author, posted $date_posted</p>
-				  <p>Message:</p>
-				  <p>$post_content</p>
+				<h2>$post_title</h2>
+				<p>Author: $author, posted $date_posted</p>
+				<p>Message:</p>
+				<p>$post_content</p>
 
-				  <h3>Leave a reply:</h3>
-				  <form action='' method='POST'>
-				  <p>Title: <br />
-				  <input type='text' name='reply_title' value='' /></p>
-				  <p>Message: <br />
-				  <textarea name='reply_message' style='width: 300pt; height: 100pt;'></textarea></p>
-				  <p><input type='submit' name='reply' value='Reply' /> <input type='reset' value='Clear' /></p>
-				  </form>
-				  ";
+				<h3>Leave a reply:</h3>
+				<form action='' method='POST'>
+				<p>Title: <br />
+				<input type='text' name='reply_title' value='' /></p>
+				<p>Message: <br />
+				<textarea name='reply_message' style='width: 300pt; height: 100pt;'></textarea></p>
+				<p><input type='submit' name='reply' value='Reply' /> <input type='reset' value='Clear' /></p>
+				</form>
+				";
       
-          //display reply posts
-          $sql = "SELECT * FROM posts WHERE reply_id='$post_id' ORDER BY date_posted desc";
-          $result = mysql_query($sql);
-          if($result) {
-            while ($row = mysql_fetch_array($result)) {
-              $title = $row['title'];
-              $author = $row['user_id'];
-              $id = $row['post_id'];
-              $content = $row['content'];
-              $date_posted = $row['date_posted'];
+        //display reply posts
+        $sql = "SELECT * FROM posts WHERE reply_id='$post_id' ORDER BY date_posted desc";
+        $result = mysql_query($sql);
+        if($result) {
+          while ($row = mysql_fetch_array($result)) {
+            $title = $row['title'];
+            $author = $row['user_id'];
+            $id = $row['post_id'];
+            $content = $row['content'];
+            $date_posted = $row['date_posted'];
             
-              echo "<h2>$title</h2>
-                    <p>Author: $author, posted $date_posted</p>
-                    <p>$content</p>";
-              
-            }
+            echo "<h2>$title</h2>
+                  <p>Author: $author, posted $date_posted</p>
+                  <p>$content</p>";
+            
           }
-		  }
-    }
+        }
+		}
 	}
 	else {
 	  echo "View Posts</p>
 	  <h2>View Posts</h2><p>First, choose a discussion:</p>";
     //admin options, add discussion if form was submitted
     if($user_type==2) {
-      $mysqli = new mysqli('132.241.49.7','dbadmin','QjpXfePqGNDfvHB79zYwand5','cins548');
+      $mysqli = new mysqli('132.241.49.7',$admin_username,$admin_password,'cins548');
       if(isset($_POST['add_discussion'])) {
         $discussion_title = $_POST['discussion_title'];
         
@@ -223,7 +224,7 @@
       }
     }
 	  //retrieve list of discussion_names
-    $mysqli = new mysqli('132.241.49.7','dbadmin','QjpXfePqGNDfvHB79zYwand5','cins548');
+    $mysqli = new mysqli('132.241.49.7',$read_username,$read_password,'cins548');
     if($result = $mysqli->query("SELECT * FROM discussions ORDER BY title")){
       while($row = $result->fetch_object()) {
 			  $title = $row->title;
